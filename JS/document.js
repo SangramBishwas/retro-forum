1597904694386b12b7b8285faf4bcb5c06a60e8f
SangramBishwas <info.sangram2003@gmail.com>
2024-03-03 15:30:21 +0600

    added interaction of first section of the main part

--- JS/document.js
+++ JS/document.js
@@ -4,47 +4,67 @@ const loadPosts = async () => {
     const posts = data.posts;
     displayPosts(posts);
 }
-const displayPosts = (posts) => {     
+const displayPosts = (posts) => {
     const postContainer = document.getElementById('post-container')
     posts.forEach(post => {
-        console.log(post)
+        // console.log(post)
         const postCard = document.createElement('div');
         postCard.classList = `flex flex-col lg:flex-row bg-[#797DFC1A] w-full p-3 lg:p-10 rounded-3xl`;
         postCard.innerHTML = `
-        <div class="">
-        <img class="w-1/12 h-1/4" src="${post.image}" alt="">
+        <div class="flex">
+        <img class="w-1/12 h-1/4 rounded" src="${post.image}" alt="">
         <div class="w-full mx-2 lg:mx-5 space-y-4">
             <div class="flex items-center w-full gap-4 lg:gap-10">
                 <span class="text-sm font-medium text-[#12132DCC]">${post.category}</span>
                 <span>Author: ${post.author.name}</span>
             </div>
-            <h3 class="text-xl font-bold">1${post.title}</h3>
+            <h3 id="post-title" class="text-xl font-bold">1${post.title}</h3>
             <p class="text-base font-normal text-[#12132D99]">${post.description}</p>
-            <div
-                class="flex items-center justify-center gap-4 lg:justify-between border-t-2 border-[#12132D40] border-dashed py-4">
+            <div class="flex items-center justify-center gap-4 lg:justify-between border-t-2 border-[#12132D40] border-dashed pt-4">
                 <div class="flex gap-5">
                     <div class="flex gap-3 items-center text-[#12132D99]">
                         <span><i class="fa-regular fa-message"></i></span>
                         <span>${post.comment_count}</span>
                     </div>
-                    <div class="flex gap-3 items-center text-[#12132D99]">
-                        <span><i class="fa-regular fa-eye"></i></span>
-                        <span>${post.view_count}</span>
+                    <div id="view-count" class="text-[#12132D99]">
+                        <span><i class="fa-regular fa-eye"></i> ${post.view_count}</span>
                     </div>
                     <div class="flex gap-3 items-center text-[#12132D99]">
                         <span><i class="fa-regular fa-clock"></i></span>
                         <span>${post.posted_time}min</span>
                     </div>
                 </div>
-                <img class="w-6 lg:w-10 cursor-pointer" src="./images/msg.png" alt="">
+                <button onclick="messageButton('${post.title}', '${post.view_count}')" class="btn"><img class="w-6 lg:w-10 cursor-pointer" src="./images/msg.png" alt="">
+                </button>
             </div>
         </div>
     </div>
 `
-    postContainer.appendChild(postCard);
-    });
-        
+        postContainer.appendChild(postCard);
 
-    
+    });
 }
+let number = 0;
+const messageButton = (title, view) => {
+    console.log('clicked', title, view);
+    number += 1;
+    getNumberById('msg-number', number);
+    const messageContainer = document.getElementById('msg-container');
+    const messageCard = document.createElement('div');
+    messageCard.classList = `card bg-base-100 shadow-xl mx-6 mb-4`;
+    messageCard.innerHTML = `
+   <div class="flex justify-between p-4">
+   <h3 class="card-title">${title}</h3>
+   <div id="view-count" class="text-[#12132D99]">
+   <span><i class="fa-regular fa-eye"></i> ${view}</span>
+   </div>
+  </div>   
+`
+    messageContainer.appendChild(messageCard);
+}
+const getNumberById = (elementId, value) => {
+    const element = document.getElementById(elementId);
+    element.innerHTML = value;
+}
+
 loadPosts()
