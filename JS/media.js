01535861f0192ba10d36a97d245811d0edf9134d
SangramBishwas <info.sangram2003@gmail.com>
2024-03-03 23:36:03 +0600

    section number three of the main part has been completed with responsive

---
+++ JS/media.js
@@ -0,0 +1,30 @@
+const loadLeteastPost = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`);
+    const posts = await res.json();
+    displayLatestPosts(posts);
+}
+const displayLatestPosts = (latestPosts) =>{
+    const latestPostContainer = document.getElementById('latest-post-container');
+    latestPosts.forEach(post => {
+        console.log(post)
+        const cards = document.createElement('div');
+        cards.classList = `card card-compact w-96 bg-base-100 shadow-xl`;
+        cards.innerHTML = `
+        <figure><img src="${post.cover_image}" alt="Shoes" /></figure>    
+        <div class="card-body">
+          <p class="text-base text-[#12132D99]">${post.author.posted_date ? post.author.posted_date : 'No publish date'}</p>
+          <h2 class="card-title font-extrabold">${post.title}</h2>
+          <p class="text-lg text-[#12132D99]">${post.description}</p>
+          <div class="flex gap-4">
+                <img class="w-[65px] h-[60px] rounded-full" src="${post.profile_image}" alt="">
+            <div>
+                <h3 class="font-bold text-lg">${post.author.name}</h3>
+                <p class="text-sm font-normal">${post.author.designation ? post.author.designation : 'Unknown'}</p>
+            </div>
+          </div>
+        </div>
+        `
+        latestPostContainer.appendChild(cards)
+    });
+}
+loadLeteastPost()
